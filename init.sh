797f05d17f8f054aa409bb3aec70acd243dc9d53
Teppei Yamaguchi <keyakko.web@gmail.com>
2018-03-23 11:29:00 +0900

    add initial script

---
+++ init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install -y \
+	tmux \
+	zsh \
+	zsh-syntax-highlighting \
+	curl \
+	tree \
+	wget \
+	tasksel \
+	openssh-server \
+	vim
+
+
+#git clone https://github.com/keyakko/common-settings ~/git/common-settings
+cp ~/git/common-settings/.* ~/
+
+
+chsh -s /bin/zsh
+
+
+pip install --user git+git://github.com/powerline/powerline
+pip install psutil
+git clone https://github.com/powerline/fonts ~/git/powerline-fonts
+~powerline-fonts/install.sh
+
