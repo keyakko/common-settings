c49112716b87a16df67c0fa6c0529734c4832394
Teppei Yamaguchi <teppei.yamaguchi@weltall.com>
2021-01-08 11:09:34 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -16,9 +16,11 @@ sudo apt install -y \
 	python3-pip \
 	network-manager 
 
-chsh -s /bin/zsh
+sudo chsh -s /bin/zsh $USER 
 
 sudo pip3 install git+git://github.com/powerline/powerline
+sudo pip3 install powerline-shell
+
 #cp -r /usr/local/lib/python3.8/dist-packages/powerline/config_files ~/.config/powerline
 #pip3 install psutil
 
@@ -30,3 +32,5 @@ git clone https://github.com/powerline/fonts ~/git/powerline-fonts --depth 1
 ~/git/powerline-fonts/install.sh
 
 cp ~/git/common-settings/.* ~/
+mkdir ~/.config/
+cp -R ~/git/common-settings/.config/powerline ~/.config/powerline
